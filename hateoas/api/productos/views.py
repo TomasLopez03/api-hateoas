076e0b4c69f7a1c4e0316f07cfb4ff998b111e74
Tomas Lopez <lopeztomas.dev@gmail.com>
2025-11-05 12:46:17 -0300

    modificación de create para poder crear varios productos a la vez

--- hateoas/api/productos/views.py
+++ hateoas/api/productos/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Producto
 from .serializers import ProductoSerializer
 from drf_spectacular.utils import  extend_schema
@@ -9,3 +10,11 @@ class ProductoViewSet(viewsets.ModelViewSet):
     """CRUD y listado para Productos con HATEOAS en el Serializer."""
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+    def create(self, request, *args, **kwargs):
+        many = isinstance(request.data, list)
+        serializer = self.get_serializer(data=request.data, many=many)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
